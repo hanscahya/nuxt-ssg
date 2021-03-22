891ff79464aaf83475309986ca55ac0127fb9795
Hans Cahya <hanscahya@users.noreply.github.com>
2021-03-22 11:13:32 +0700

    Init

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>hello</h1>
+        <p>{{ res }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data () {
+    return {
+      res: null
+    }
+  },
+  async mounted () {
+    await this.$axios.get('content/home').then(response => {
+      if (response.status !== 200) throw new Error(response.data.message.reason)
+      else this.res = response.data
+    })
+  }
+})
+</script>
+
+<style>
+</style>
